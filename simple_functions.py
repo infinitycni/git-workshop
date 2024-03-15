7f9ff1dfb6d7311074041ff2bbf564af64c1d325
JahnaviEdara3 <jahnavi.edara@tigeranalytics.com>
2024-03-15 13:23:34 +0530

    ENH:Adding docstring to the functions

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,11 @@
 # Custom python functions
 
 def double_number(a):
+    '''Addition of the same number.
+    param int a'''
     return a+a
 
 def square_number(a):
+    '''Squaring of the same number.
+    param int a'''
     return a*a
