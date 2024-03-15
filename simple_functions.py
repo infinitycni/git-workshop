c02a543e623ea338219aa30fb4ffb89f9bd9a032
SumyamiMarla <padma.marla@tigeranalytics.com>
2024-03-15 15:22:15 +0530

    ENH:Add the print statement

--- simple_functions.py
+++ simple_functions.py
@@ -8,4 +8,4 @@ def double_number(a):
 def square_number(a):
     print(f'value before square_number(): {a}')
     result=a*a
-print(f'value after square_number(): {result}')
+    print(f'value after square_number(): {result}')
