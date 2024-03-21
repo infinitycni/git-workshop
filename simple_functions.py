955d4fa768033b0f0d1e73ba5d2a2926a8755f15
infinitycni <swetha.elangovan@tigeranalytics.com>
2024-03-21 12:11:17 +0530

    updated

--- simple_functions.py
+++ simple_functions.py
@@ -1,11 +1,18 @@
 # Custom python functions
 
 def double_number(a):
+    '''Addition of the same number.
+    param int a'''
     print(f'value before double_number(): {a}')
     result=a+a
     print(f'value after double_number(): {result}')
+    return result
+
 
 def square_number(a):
+     '''Squaring of the same number.
+    param int a'''
     print(f'value before square_number(): {a}')
     result=a*a
     print(f'value after square_number(): {result}')
+    return result
